d428b93babaf8de7f8078780cf7301172069ce9f
vaujan <fauzan20014@gmail.com>
2024-11-20 11:56:33 +0700

    small update: add new class constructor and draw it to screen. fix: correct Player class implementation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
+from circleshape import *
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,13 +9,26 @@ def main():
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
-
+    
+    game_clock = pygame.time.Clock()
+    dt = 0
+       
+    # Render the game
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
+
+        # Spawning player after filling the screen with "black"
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
+        
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
