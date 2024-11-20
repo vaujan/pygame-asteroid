c8db331b0d829340d69bea4775dfff6641ea9f6f
vaujan <fauzan20014@gmail.com>
2024-11-20 17:26:47 +0700

    update: add new random incoming asteroid. add sprint feature.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from circleshape import *
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     pygame.init()
@@ -9,11 +11,21 @@ def main():
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
-    
-    game_clock = pygame.time.Clock()
+
+    clock = pygame.time.Clock()
     dt = 0
-       
-    # Render the game
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroid = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (asteroid, updateable, drawable)
+    AsteroidField.containers = (updateable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
     while True: 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -21,13 +33,15 @@ def main():
 
         screen.fill("black")
 
-        # Spawning player after filling the screen with "black"
-        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        for sprite in updateable:
+            sprite.update(dt)
         
         pygame.display.flip()
 
-        dt = game_clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -17,3 +17,28 @@ class Player(CircleShape):
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+    
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+    
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_a]:
+            self.rotate(-dt)       
+        if keys[pygame.K_d]:
+            self.rotate(dt)       
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+        if keys[pygame.K_r]:
+            self.position = pygame.Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        
+        if keys[pygame.K_SPACE]:
+            self.move(dt * 2)
